47cb09f3ca5ee7af7795315ec8ad5df553742c2f
Andrue Peters <5511409+AndruePeters@users.noreply.github.com>
2019-07-01 16:46:56 -0400

    Changes /etc/apt/sources.list, but is currently set for a test directory

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
 #!/usr/bin/env bash
+
+# first step is to upgrade machine to debian testing
+sources="deb http://ftp.us.debian.org/debian/ testing main\n
+deb-src http://ftp.us.debian.org/debian/ testing main\n
+deb http://security.debian.org/debian-security testing/updates main\n
+deb-src http://security.debian.org/debian-security testing/updates main\n
+deb http://ftp.us.debian.org/debian/ testing-updates main\n
+deb-src http://ftp.us.debian.org/debian/ testing-updates main"
+
+dest="./test.txt"
+echo -e $sources > $dest
